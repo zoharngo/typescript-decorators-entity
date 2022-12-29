4904adecd54c39a6f90f105ec95fb8e343d54137
zoharngo <zoharngo@gmail.com>
2022-12-29 16:38:14 +0200

    add fake secert

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import Person from './entities/Person';
 
 export const apiServer = new APIServer();
 export const db = new JsonDB(new Config("entityDatabase", true, true, '/'));
-
+export const AWS_SQS_ACCESS_KEY_ID = 'QEDFFDKK6SM354DHT';
 apiServer.addEntity<Person>(Person);
 
 apiServer.start();
